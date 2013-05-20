d40605bfc5f158ec1e26656f773cbb3a7ed4a1b2
Silence <jinjin@server.local>
2013-05-20 17:17:52 +0300

    pipe

--- realtime_project/test.c
+++ realtime_project/test.c
@@ -11,7 +11,7 @@ void initial_time(struct timespec* time)
 	time->tv_nsec=0;
 }
 
-void start_read(int* sensorDescriptor){
+struct timespec start_read(int* sensorDescriptor){
 	Tmeas measurement[NUMOFVALUE];
 	struct timespec diff,sum;
 	int i;
@@ -25,11 +25,13 @@ void start_read(int* sensorDescriptor){
 			printf("time value was %lld.%.9ld\n",diff.tv_sec,diff.tv_nsec );
 			//printf("test sum value was %lld.%.9ld\n",sum.tv_sec,getnanosec(&sum));
 			increment_timespec(&sum,&diff);
-
-			printf("pid: %d sum value was %lld.%.9ld\n",sum.tv_sec,sum.tv_nsec,getpid());
+			printf("pid: %d sum value was %lld.%.9ld\n",getpid(),sum.tv_sec,sum.tv_nsec);
+			
 		}
 
 	}
+	//printf("pid: %d sum value was %lld.%.9ld\n",getpid(),sum.tv_sec,sum.tv_nsec);
+	return sum;
 }
 
 int main(void)
@@ -37,26 +39,49 @@ int main(void)
 	int noOfSensors,i=0;
 	pid_t pid;
 	int sensorDescriptor [20];
+	int status;
+	int pipe_arr[2];
+	struct timespec return_csum,total_sum,return_psum;
+	initial_time(&total_sum);
+	pipe(pipe_arr);
 	
-
 	noOfSensors = StartSimulator(sensorDescriptor, NUMOFVALUE);
 
-    while (i < 5) {
-    pid = fork();
-    i++;
+    while (i < 19) {
+    //i++;
+	//printf("test\n");
+	pid = fork();
+  
+	//printf("%d\n",getpid());
     if (pid == 0) { // this is child
         // this is child.
         // tasks of child process are performed.
-    	start_read(&sensorDescriptor[i-1]);
-        exit(0);    // child terminates.
+		close(pipe_arr[0]);
+		return_csum=start_read(&sensorDescriptor[i]);
+    	write(pipe_arr[1],&return_csum,sizeof(return_csum));
+		//printf("child: %d    childpid:%d\n\n",i,getpid());
+		exit(0);
+        //exit(0);    // child terminates.
         }
-        
+        i++;
+		close(pipe_arr[1]);
+		read(pipe_arr[0],&return_csum,sizeof(return_csum));
+		increment_timespec(&total_sum,&return_csum);
+		//printf("child total time value was %lld.%.9ld\n",total_sum.tv_sec,total_sum.tv_nsec );
         // Parent continues from here after creating   // a child.
-        start_read(&sensorDescriptor[4]);
-    } 
+        //
+		//printf("parent1 %d     parentpid:%d \n",i,getpid());
+		
+    }
+	//i++;
+	
+	return_psum=start_read(&sensorDescriptor[i]);
+	increment_timespec(&total_sum,&return_psum);
+	printf("parent total time value was %lld.%.9ld\n",total_sum.tv_sec,total_sum.tv_nsec );
+	//printf("parent %d     parentpid:%d \n\n",i,getpid());
     // Parent continues from here after creating // all children.
     // Tasks of parent process  are performed.
-    while(wait(NULL) > 0);
+    while(waitpid(-1,NULL,WNOHANG) > 0);
     exit(0);
 
 	return 0;
