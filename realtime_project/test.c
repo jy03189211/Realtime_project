7e245285479909d91d4ef7f0c384cba4a5091a8a
Silence <jinjin@server.local>
2013-05-22 18:45:33 +0300

    all done

--- realtime_project/test.c
+++ realtime_project/test.c
@@ -19,13 +19,13 @@ struct timespec start_read(int* sensorDescriptor){
 	int i;
 	initial_time(&sum);
 
+
 	for(i=0;i<NUMOFVALUE;i++){
 		read(*sensorDescriptor, &measurement[i], sizeof(Tmeas));
 		printf("Measurement value was %d\n", measurement[i].value);
 		if(i>0){
 			diff=diff_timespec(&measurement[i-1].moment,&measurement[i].moment);
-			//printf("time value was %lld.%.9ld\n",diff.tv_sec,diff.tv_nsec );
-			//printf("test sum value was %lld.%.9ld\n",sum.tv_sec,getnanosec(&sum));
+			
 			increment_timespec(&sum,&diff);
 			
 			
@@ -43,9 +43,11 @@ int main(void)
 	int sensorDescriptor [20];
 	int status;
 	int pipe_arr[2];
-	struct timespec return_csum,total_sum,return_psum;
+	struct timespec return_csum,total_sum,return_psum,csum_max,csum_min;
 	initial_time(&total_sum);
 	initial_time(&return_csum);
+	initial_time(&csum_max);
+	initial_time(&csum_min);
 	pipe(pipe_arr);
 
 
@@ -69,20 +71,7 @@ int main(void)
         //exit(0);    // child terminates.
         }
         i++;
-        //close(pipe_arr[1]);
-        //flag=fcntl(pipe_arr[0],F_GETFL);
-        //fcntl(pipe_arr[0],F_SETFL,flag|O_NONBLOCK);
-		
-		//read(pipe_arr[0],&return_csum,sizeof(return_csum));
-
-
-		//increment_timespec(&total_sum,&return_csum);
-		//printf("child total time value was %lld.%.9ld\n",return_csum.tv_sec,return_csum.tv_nsec );
-		
-		//printf("child total time value was %lld.%.9ld\n",total_sum.tv_sec,total_sum.tv_nsec );
-        // Parent continues from here after creating   // a child.
-        //
-		//printf("parent1 %d     parentpid:%d \n",i,getpid());
+        
 		
     }
     // Parent continues from here after creating
@@ -90,18 +79,73 @@ int main(void)
     // Tasks of parent process  are performed.
 	//sum up children
 	close(pipe_arr[1]);
-	while(read(pipe_arr[0],&return_csum,sizeof(return_csum))>0)
+
+	while(read(pipe_arr[0],&return_csum,sizeof(return_csum))>0){
 		increment_timespec(&total_sum,&return_csum);
-	printf("child total time value was %lld.%.9ld\n",total_sum.tv_sec,total_sum.tv_nsec );
+		//bubble compare
+		//calculate csum_max
+		if (return_csum.tv_sec>csum_max.tv_sec)
+			csum_max=return_csum;
+		else if (return_csum.tv_sec==csum_max.tv_sec)	
+		{
+			if (return_csum.tv_nsec>csum_max.tv_nsec)
+				csum_max=return_csum;
+			
+		}
+		//initial min
+		if (csum_min.tv_sec==0&&csum_min.tv_nsec==0)
+		{
+			csum_min=return_csum;
+		}
+		//calculate csum_min
+		if (return_csum.tv_sec<csum_min.tv_sec)
+			csum_min=return_csum;
+		else if (return_csum.tv_sec==csum_min.tv_sec)	
+		{
+			if (return_csum.tv_nsec<csum_min.tv_nsec)
+				csum_min=return_csum;
+		}			
+	}
+	//printf("max and min value was %lld.%.9ld  %lld.%.9ld\n",csum_max.tv_sec,csum_max.tv_nsec,csum_min.tv_sec,csum_min.tv_nsec );
+
+
+	//printf("child total time value was %lld.%.9ld\n",total_sum.tv_sec,total_sum.tv_nsec );
 	//sum up with parent
 	return_psum=start_read(&sensorDescriptor[i]);
 	increment_timespec(&total_sum,&return_psum);
 	//print parent
-	printf("parent  time value was %lld.%.9ld\n",return_psum.tv_sec,return_psum.tv_nsec );
+	//printf("parent  time value was %lld.%.9ld\n",return_psum.tv_sec,return_psum.tv_nsec );
 	//print total
-	printf("total time value was %lld.%.9ld\n",total_sum.tv_sec,total_sum.tv_nsec );
+
+	//compare child and parent for max
+	printf("\n\n");
+	if (csum_max.tv_sec>return_psum.tv_sec)
+	{
+		printf("max  value was %lld.%.9ld\n",csum_max.tv_sec,csum_max.tv_nsec);
+	}
+	else if(csum_max.tv_sec==return_psum.tv_sec){
+		if (csum_max.tv_nsec>return_psum.tv_nsec)
+			printf("max  value was %lld.%.9ld\n",csum_max.tv_sec,csum_max.tv_nsec);
+		else
+			printf("max  value was %lld.%.9ld\n",return_psum.tv_sec,return_psum.tv_nsec);
+	}else
+		printf("max  value was %lld.%.9ld\n",return_psum.tv_sec,return_psum.tv_nsec);
+	//compare child and paret for min
+	if (csum_min.tv_sec<return_psum.tv_sec)
+	{
+		printf("min  value was %lld.%.9ld\n",csum_min.tv_sec,csum_min.tv_nsec);
+	}
+	else if(csum_min.tv_sec==return_psum.tv_sec){
+		if (csum_min.tv_nsec<return_psum.tv_nsec)
+			printf("min  value was %lld.%.9ld\n",csum_min.tv_sec,csum_min.tv_nsec);
+		else
+			printf("min  value was %lld.%.9ld\n",return_psum.tv_sec,return_psum.tv_nsec);
+	}else
+		printf("min  value was %lld.%.9ld\n",return_psum.tv_sec,return_psum.tv_nsec);
+
+
+	printf("total time value was %lld.%.9ld  average delay is %lld.%.9ld\n",total_sum.tv_sec,total_sum.tv_nsec,total_sum.tv_sec/(NUMOFCHILDREN+1),total_sum.tv_nsec/(NUMOFCHILDREN+1) );
 	
-	//printf("parent %d     parentpid:%d \n\n",i,getpid());
 
     while(wait(NULL) > 0);
     exit(0);
